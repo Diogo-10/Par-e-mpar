2783980709a8a1d09b4c2450d747943a5167f2d4
Diogo-10 <diogogf@hotmail.com>
2020-06-15 18:24:54 -0300

    impar e par

---
+++ src/imp/e/pr/Condcionalpareimpar.java
@@ -0,0 +1,18 @@
+
+package imp.e.pr;
+import java.util.Scanner;
+public class ImpEPr {
+
+   
+    public static void main(String[] args) {
+            Scanner entrada = new Scanner (System.in);
+        System.out.println ("Digite um número:");
+        int n = entrada.nextInt();
+        if (n % 2 == 0)
+            System.out.println("Par");
+        else
+            System.out.println("Impar");
+        
+    }
+    
+}
